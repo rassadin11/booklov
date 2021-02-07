6d3ff144f6cc683dde5b08d1c4163e1f25d64bd6
rassadin11 <58170052+rassadin11@users.noreply.github.com>
2021-02-07 10:24:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,82 @@
+const animItems = document.querySelectorAll('.anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if (!animItem.classList.contains('prove')) {
+				if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+					animItem.classList.add('active');
+					animItem.classList.add('prove');
+				} else {
+					animItem.classList.remove("active");
+				}
+			}
+		}
+	}
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
+			  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+		return {
+			top: rect.top + scrollTop,
+			left: rect.left + scrollLeft,
+		}
+	}
+
+	animOnScroll();
+};
+
+if (document.body.clientWidth <= 768) {
+	let elems = document.querySelectorAll('.advantage');
+	if (elems.length % 2 != 0) {
+		elems[elems.length - 1].classList.add('active');
+	}
+}
+
+let section = document.querySelector('.section');
+
+if (document.body.clientWidth > 1024) {
+	section.addEventListener('mousemove', (event) => {
+		let target = event.target;
+			partItem = document.querySelectorAll('.part-item__main-icon');
+
+		for (let elem of partItem) {
+			if (target == elem) {
+				let figure = elem.parentElement.parentElement;
+					figureElem = figure.querySelector('.part-item__oval')
+				if (figureElem) {
+					figureElem.classList.add('hover')
+				}
+			}
+		}
+	})
+
+	section.addEventListener('mouseout', (event) => {
+		let target = event.target;
+			partItem = document.querySelectorAll('.part-item__main-icon');
+
+		for (let elem of partItem) {
+			if (target == elem) {
+				let figure = elem.parentElement.parentElement;
+					figureElem = figure.querySelector('.part-item__oval')
+				if (figureElem.classList.contains('hover')) {
+					figureElem.classList.remove('hover')
+				}
+			}
+		}
+	})
+}
